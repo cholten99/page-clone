bb2a8b89ab51a6f6ff41007debdba292218cf010
David Durant <dave@bowsy.co.uk>
2013-04-18 03:39:15 +0100

    Last of tidying

--- remote.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <head>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="HelpGuy.js"></script>
-  </head>
-  <body onload="InitialPageLoadAndPoll('<?php print($_GET['UID']); ?>')">
-  </body>
-</html>
