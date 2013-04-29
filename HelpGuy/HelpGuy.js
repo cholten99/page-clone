e125bd5554501ba072c06d30f7f42f87cc19834d
David Durant <dave@bowsy.co.uk>
2013-04-29 03:03:40 +0100

    Minor update to HelpGuy JS and a bunch of stuff in the documentation

--- HelpGuy/HelpGuy.js
+++ HelpGuy/HelpGuy.js
@@ -11,41 +11,38 @@ function Login() {
 
 // Poll every two seconds for updates
 function PollSessionRequests() {
-  if (HelperUID != "") {
 
-    $.get('../Common/CRUD.php', { Function: 'GetAllRowsForFields', UserUID: 0 }, function(data) {
+  $.get('../Common/CRUD.php', { Function: 'GetAllRowsForFields', UserUID: 0 }, function(data) {
 
-      var nameListArray = jQuery.parseJSON(data);
-      var numberOfUsersWaiting = nameListArray.length;
+    var nameListArray = jQuery.parseJSON(data);
+    var numberOfUsersWaiting = nameListArray.length;
 
-      $('#TopText').html("Number of open requests: " + numberOfUsersWaiting + "<p>");
+    $('#TopText').html("Number of open requests: " + numberOfUsersWaiting + "<p>");
 
-      // Don't even show the queue if there's nothing to show
-      if (numberOfUsersWaiting != 0) {
+    // Don't even show the queue if there's nothing to show
+    if (numberOfUsersWaiting != 0) {
 
-        $('#ListHeaders').html("<div id='ListHeader'>Session Keyword</div>");
+      $('#ListHeaders').html("<div id='ListHeader'>Session Keyword</div>");
 
-        var htmlString = "";
-        var odd = true;
-        for (var i=0; i < numberOfUsersWaiting; i++) {
-          var userUID = nameListArray[i]['UserUID'];
+      var htmlString = "";
+      var odd = true;
+      for (var i=0; i < numberOfUsersWaiting; i++) {
+        var userUID = nameListArray[i]['UserUID'];
 
-          var remoteTabURL = "http://bowsy.me.uk/PageClone/RemoteTab/index.php?UserUID=" + userUID;
+        var remoteTabURL = "http://bowsy.me.uk/PageClone/RemoteTab/index.php?UserUID=" + userUID;
 
-          var divContents = "<div id='" + odd + "'><a target='_blank' href='" + remoteTabURL + "'>" + userUID + "</a></div>";
+        var divContents = "<div id='" + odd + "'><a target='_blank' href='" + remoteTabURL + "'>" + userUID + "</a></div>";
 
-          htmlString += "<div id='ListRow'>" + divContents + "</div><p>";
-          odd ? odd = false : odd = true;
-        }
-        $('#Users').html(htmlString);
-
+        htmlString += "<div id='ListRow'>" + divContents + "</div><p>";
+        odd ? odd = false : odd = true;
       }
+      $('#Users').html(htmlString);
 
-      // Loop around again every 2 seconds (forever) to set up another long-response AJAX call
-      setTimeout("PollSessionRequests();", 2000)
+    }
 
-    }); 
+    // Loop around again every 2 seconds (forever) to set up another long-response AJAX call
+    setTimeout("PollSessionRequests();", 2000)
 
-  }
+  }); 
 
 }
